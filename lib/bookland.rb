a5233e5dbe2771362ece9224e43a3f98edc88bfb
Piotr Laszewski and Hakan Ensari <we.write@papercavalier.com>
2010-04-21 16:17:13 +0100

    to_s up to be fixed

--- lib/bookland.rb
+++ lib/bookland.rb
@@ -1,148 +1,89 @@
 module Bookland
-  
-  # most methods take an Array containing ISBN digits.
-  # 'cd' means 'check digit'.
-
-  def cd10(raw)
-    seed, cd = raw[0..8], raw[9]
-    v = 11 - [10, 9, 8, 7, 6, 5, 4, 3, 2].zip(seed).map { |n,m| n * m }.inject(0) { |i,j| i+j } % 11
-  end
-
-  def cd13(raw)
-    seed, cd = raw[0..11], raw[12]
-    v = (10 - [1, 3, 1, 3, 1, 3, 1, 3, 1, 3, 1, 3].zip(seed).map { |n,m| n * m }.inject(0){ |i,j| i+j } % 10) % 10
-  end
-
-  def put_cd10(raw)
-    raw[0..8] + [cd10(raw)]
-  end
-
-  def put_cd13(raw)
-    raw[0..11] + [cd13(raw)]
-  end
-
-  def put_cd(raw)
-    by_length(raw, "put_cd10(raw)", "put_cd13(raw)")
-  end
-
-  def to_isbn(raw)
-    by_length(raw, "cd10(raw)", "cd13(raw)")
-  end
-
-  def by_length(raw, do10, do13)
-    if raw.length < 11
-      eval do10
-    else
-      eval do13
+  class ISBN
+    def initialize(seed)
+      @raw = seed.length == 0 ? [] : put_cd(seed.gsub(/[Xx]/, '0').gsub(/[^0-9]/, '').split(//).map { |c| c.to_i })
     end
-  end
 
-  def isbnchar(i) # needed only for ISBN-10
-    case i
-    when String then i
-    when 0..9 then i.to_s
-    when 10 then 'X'
-    when 11 then '0'
-    else '*'
+    def valid?
+      false
     end
-  end
-end
-
-class String
-  def isbn_string
-    gsub(/[Xx]/, '0').gsub(/[^0-9]/, '')
-  end
-
-  def to_isbn
-    ISBN.new(self)
-  end
-end
-
-class Integer
-  def digit(base)
-    n, c = self, 0
-    n, c = n/base, c+1 while n >= 1
-    return c
-  end
-
-  # Integer -> ISBN
-  def to_isbn
-    ISBN.new(self)
-  end
-end
-
-class Array
-  include Bookland
   
-  def to_isbn
-    inject('') { |i,j| isbnchar(i) + isbnchar(j) }.to_isbn
-  end
-  
-  def map_accum
-    raw = []
-    each_index { |i| raw << self[0..i].inject(0) { |n,m| n+m } }
-    return raw
-  end
-end
+    def to_isbn10
+      ISBN.new(by_length(@raw,"put_cd10(@raw[0..9])", "put_cd10(@raw[3..11])").to_s)
+    end
 
-class ISBN
-  include Bookland
-  attr_accessor :raw
+    def to_isbn13
+      ISBN.new(by_length(@raw, "put_cd13([9,7,8] + @raw[0..8])", "put_cd13(@raw[0..11])").to_s)
+    end
 
-  def initialize(*seed)
-    seed = seed.to_s
-    if seed.length == 0
-      @raw
-    else
-      case seed[0]
-        when String then d,s = seed[0].length, seed[0].isbn_string.to_i
-        when Integer then d,s = seed[0].digit(10), s = seed[0]
+    def ==(other)
+      self.to_isbn13.to_s == other.to_isbn13.to_s
+    end
+
+    def to_s(*blocks)
+      return "okj"
+      return 1
+      if blocks.length == 0
+        return put_cd(@raw)
       end
-      @raw = put_cd(Array.new(d <= 10 ? 10 : 13) { |i| 10**i }.reverse.map { |ord| s / ord % 10 })
+            # 
+            # mark = '-'
+            # if blocks.length == 0
+            #   hyphenate = nil
+            # else
+            #   case blocks.last
+            #     when String then mark = blocks.pop
+            #   end
+            # end
+            # 
+            # raw = @raw.dup
+            # l = raw.length-1
+            # positions = blocks.map_accum.delete_if { |x| x > l || x <= 0 }
+            # positions.push l unless positions.last == l || hyphenate.nil?
+            # positions.zip(Array.new(raw.length) { |i| i }).map { |i, j| i + j }.each do |b|
+            #   raw.insert(b, mark)
+            # end
+            # raw.inject('') { |i,j| isbnchar(i) + isbnchar(j) }
     end
-  end
+    
+    private
+    
+    def cd10(raw)
+      seed, cd = raw[0..8], raw[9]
+      v = 11 - [10, 9, 8, 7, 6, 5, 4, 3, 2].zip(seed).map { |n,m| n * m }.inject(0) { |i,j| i + j } % 11
+    end
 
-  def valid?
-    false
-  end
-  
-  def inspect
-    raw.inject('') { |i,j| isbnchar(i) + isbnchar(j) }
-  end
+    def cd13(raw)
+      seed, cd = raw[0..11], raw[12]
+      v = (10 - [1, 3, 1, 3, 1, 3, 1, 3, 1, 3, 1, 3].zip(seed).map { |n,m| n * m }.inject(0) { |i,j| i + j } % 10) % 10
+    end
 
-  # ISBN -> ISBN
-  def to_isbn10
-    raw = self.raw
-    by_length(raw,"put_cd10(raw[0..9])", "put_cd10(raw[3..11])").to_isbn
-  end
-  def to_isbn13
-    raw = self.raw
-    by_length(raw, "put_cd13([9,7,8] + raw[0..8])", "put_cd13(raw[0..11])").to_isbn
-  end
+    def put_cd10(raw)
+      raw[0..8] + [cd10(raw)]
+    end
 
-  def ==(other)
-    self.isbn13.to_s == other.isbn13.to_s
-  end
+    def put_cd13(raw)
+      raw[0..11] + [cd13(raw)]
+    end
 
-  # ISBN -> String
-  def to_s(*blocks)
-    mark = '-'
-    if blocks.length == 0
-      hyphenate = nil
-    else
-      case blocks.last
-        when String then mark = blocks.pop
+    def put_cd(raw)
+      return "ok"
+      by_length(raw, "put_cd10(raw)", "put_cd13(raw)")
+    end
+
+    def by_length(raw, do10, do13)
+      eval raw.length < 11 ? do10 : do13
+    end
+
+    def isbnchar(i) # needed only for ISBN-10
+      case i
+      when String then i
+      when 0..9 then i.to_s
+      when 10 then 'X'
+      when 11 then '0'
+      else '*'
       end
     end
-
-    raw = self.raw.dup
-    l = raw.length-1
-    positions = blocks.map_accum.delete_if { |x| x > l || x <= 0 }
-    positions.push l unless positions.last == l || hyphenate.nil?
-    positions.zip(Array.new(raw.length) { |i| i }).map{|i,j| i+j}.each do |b|
-      raw.insert(b, mark)
-    end
-    raw.inject('') { |i,j| isbnchar(i) + isbnchar(j) }
   end
 end
 
--- spec/bookland_spec.rb
+++ spec/bookland_spec.rb
@@ -1,67 +1,81 @@
 require "spec_helper"
 
-describe "Bookland" do
-  it "should convert an ISBN-10 to ISBN-13" do
-    isbns do |isbn10, isbn13|
-      ISBN.new(isbn10).to_isbn13.should == ISBN.new(isbn13)
+module Bookland
+  describe ISBN do
+    
+    
+    
+    it "should cast as string" do
+      ISBN.new('0262011530').to_s.should == '0262011530'
     end
-  end
-  
-  it "should convert an ISBN-13 to ISBN-10" do
-    isbns do |isbn10, isbn13|
-      ISBN.new(isbn13).to_isbn10.should == ISBN.new(isbn10)
+    
+    it "should cast as string and format" do
+      p ISBN.new('0262011530').to_s(1, 3, 5)
+      ISBN.new('0262011530').to_s(1, 3, 5).should == '0-262-01153-0'
     end
-  end
-  
-  it "should equate ISBN-10 and ISBN-13" do
-    isbns do |isbn10, isbn13|
-      ISBN.new(isbn10).should == ISBN.new(isbn13)
+    
+    it "should convert an ISBN-10 to ISBN-13" do
+      isbns do |isbn10, isbn13|
+        ISBN.new(isbn10).to_isbn13.to_s.should == isbn13
+      end
     end
-  end
   
-  it "should validate valid ISBN-10s" do
-    isbns do |isbn10, isbn13|
-      ISBN.new(isbn10).valid?.should be_true
-    end
-  end
-  
-  it "should validate valid ISBN-13s" do
-    isbns do |isbn10, isbn13|
-      ISBN.new(isbn13).valid?.should be_true
-    end
-  end
-  
-  it "should not validate if seed looks like an ISBN-13 but has an invalid check digit" do
-    isbns do |isbn10, isbn13|
-      invalid = isbn13.gsub(/(.)$/, (('0'..'9').to_a - ['\1'])[rand(10)])
-      ISBN.new(invalid).valid?.should be_false
-    end
-  end
-  
-  it "should not validate if seed looks like an ISBN-10 but has an invalid check digit" do
-    isbns do |isbn10, isbn13|
-      invalid = isbn10.gsub(/(.)$/, (('0'..'9').to_a + ['X'] - ['\1'])[rand(11)])
-      ISBN.new(invalid).valid?.should be_false
-    end
-  end
-  
-  it "should not validate if seed is not an ISBN" do
-    %w{foo 1}.each { |seed| ISBN.new(seed).valid?.should be_false }
-  end
-  
-  it "should not validate if seed is blank" do
-    ISBN.new('').valid?.should be_false
-  end
-  
-  it "should not validate if seed is nil" do
-    ISBN.new(nil).valid?.should be_false
-  end
-  
-  it "should hyphenate an ISBN-13" do
-    ISBN.new("9780485113358").to_s(3, 10, '-').should == '978-0485113358'
-  end
-  
-  it "should hyphenate an ISBN-10" do
-    ISBN.new("9780485113358").to_isbn10.to_s(1, 3, 5, 1, '-').should == '0-485-11335-X'
+    it "should convert an ISBN-13 to ISBN-10" do
+       isbns do |isbn10, isbn13|
+         ISBN.new(isbn13).to_isbn10.should == ISBN.new(isbn10)
+       end
+     end
+     
+     it "should equate ISBN-10 and ISBN-13" do
+       isbns do |isbn10, isbn13|
+         ISBN.new(isbn10).should == ISBN.new(isbn13)
+       end
+     end
+   
+     it "should validate valid ISBN-10s" do
+       isbns do |isbn10, isbn13|
+         ISBN.new(isbn10).valid?.should be_true
+       end
+     end
+   
+     it "should validate valid ISBN-13s" do
+       isbns do |isbn10, isbn13|
+         ISBN.new(isbn13).valid?.should be_true
+       end
+     end
+   
+     it "should not validate if seed looks like an ISBN-13 but has an invalid check digit" do
+       isbns do |isbn10, isbn13|
+         invalid = isbn13.gsub(/(.)$/, (('0'..'9').to_a - ['\1'])[rand(10)])
+         ISBN.new(invalid).valid?.should be_false
+       end
+     end
+   
+     it "should not validate if seed looks like an ISBN-10 but has an invalid check digit" do
+       isbns do |isbn10, isbn13|
+         invalid = isbn10.gsub(/(.)$/, (('0'..'9').to_a + ['X'] - ['\1'])[rand(11)])
+         ISBN.new(invalid).valid?.should be_false
+       end
+     end
+   
+     it "should not validate if seed is not an ISBN" do
+       %w{foo 1}.each { |seed| ISBN.new(seed).valid?.should be_false }
+     end
+   
+     it "should not validate if seed is blank" do
+       ISBN.new('').valid?.should be_false
+     end
+   
+     it "should not validate if seed is nil" do
+       ISBN.new(nil).valid?.should be_false
+     end
+   
+     it "should hyphenate an ISBN-13" do
+       ISBN.new("9780485113358").to_s(3, 10, '-').should == '978-0485113358'
+     end
+   
+     it "should hyphenate an ISBN-10" do
+       ISBN.new("9780485113358").to_isbn10.to_s(1, 3, 5, 1, '-').should == '0-485-11335-X'
+     end
   end
 end
