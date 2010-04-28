c128966f671b2ec10b4acf518c416cdc4d4cdb74
Hakan Ensari <hakan.ensari@papercavalier.com>
2010-04-28 12:21:20 +0100

    Can now create invalid ISBN instances

--- lib/bookland.rb
+++ lib/bookland.rb
@@ -1,45 +1,67 @@
 module Bookland
+
+  # A simple ISBN class
   class ISBN
-    def initialize(seed)
-      @raw = seed.gsub(/[^Xx0-9]/, '').split(//) rescue @raw = []
-      raise ISBNError unless valid?
+    def ==(other)
+      self.to_isbn13.to_s == other.to_isbn13.to_s
     end
-    
+
+    def initialize(seed=nil)
+      self.seed = seed
+    end
+
     def inspect
       to_s
     end
 
+    def seed=(seed)
+      @raw = seed.gsub(/[^Xx0-9]/, '').split(//) rescue @raw = []
+    end
+
     def to_isbn10
+      raise ISBNError unless valid?
+
       if isbn13?
         raw = @raw[3..11]
-        ISBN.new((raw << cd10(raw)).to_s)
+        ISBN.new((raw << check_digit_10(raw)).to_s)
       else
         self.dup
       end
     end
 
     def to_isbn13
+      raise ISBNError unless valid?
+
       if isbn10?
         raw = @raw[0..8].unshift('9', '7', '8')
-        ISBN.new((raw << cd13(raw)).to_s)
+        ISBN.new((raw << check_digit_13(raw)).to_s)
       else
         self.dup
       end
     end
 
-    def ==(other)
-      self.to_isbn13.to_s == other.to_isbn13.to_s
-    end
-
     def to_s(*blocks)
+      raise ISBNError unless valid?
+
       raw = @raw.dup
       blocks.any? ? (blocks.map { |i| raw.shift(i).to_s } << raw.to_s).delete_if(&:empty?).join('-') : raw.to_s
     end
 
+    def valid?
+      if isbn10?
+        @raw[9] == check_digit_10(@raw)
+      elsif isbn13?
+        @raw[12] == check_digit_13(@raw)
+      else
+        false
+      end
+    end
+
     private
 
-    def cd10(raw)
+    def check_digit_10(raw)
       cd = 11 - 10.downto(2).to_a.zip(raw[0..8].map(&:to_i)).map { |n,m| n * m }.inject(0) { |i,j| i + j } % 11
+
       case cd
       when 0..9 then cd.to_s
       when 10 then 'X'
@@ -47,7 +69,7 @@ module Bookland
       end
     end
 
-    def cd13(raw)
+    def check_digit_13(raw)
       ((10 - ([1, 3] * 6).zip(raw[0..11].map(&:to_i)).map { |n,m| n * m }.inject(0) { |i,j| i + j } % 10) % 10).to_s
     end
 
@@ -58,18 +80,9 @@ module Bookland
     def isbn13?
       @raw.length == 13
     end
-
-    def valid?
-      if isbn10?
-        @raw[9] == cd10(@raw)
-      elsif isbn13?
-        @raw[12] == cd13(@raw)
-      else
-        false
-      end
-    end
   end
 
+  # A simple error wrapper for a simple ISBN class
   class ISBNError < StandardError
     def initialize(msg='ISBN not valid')
       super
--- spec/bookland_spec.rb
+++ spec/bookland_spec.rb
@@ -9,65 +9,75 @@ module Bookland
     end
 
     it "should convert an ISBN-13 to ISBN-10" do
-       isbns do |isbn10, isbn13|
-         ISBN.new(isbn13).to_isbn10.should == ISBN.new(isbn10)
-       end
-     end
+      isbns do |isbn10, isbn13|
+        ISBN.new(isbn13).to_isbn10.should == ISBN.new(isbn10)
+      end
+    end
 
-     it "should equate ISBN-10 and ISBN-13" do
-       isbns do |isbn10, isbn13|
-         ISBN.new(isbn10).should == ISBN.new(isbn13)
-       end
-     end
+    it "should equate ISBN-10 and ISBN-13" do
+      isbns do |isbn10, isbn13|
+        ISBN.new(isbn10).should == ISBN.new(isbn13)
+      end
+    end
 
-     it "should validate ISBN-10s" do
-       isbns do |isbn10, isbn13|
-         lambda { ISBN.new(isbn10) }.should_not raise_error
-       end
-     end
+    it "should validate ISBN-10s" do
+      isbns do |isbn10, isbn13|
+        ISBN.new(isbn10).valid?.should be_true
+      end
+    end
 
-     it "should validate ISBN-13s" do
-       isbns do |isbn10, isbn13|
-         lambda { ISBN.new(isbn13) }.should_not raise_error
-       end
-     end
+    it "should validate ISBN-13s" do
+      isbns do |isbn10, isbn13|
+        ISBN.new(isbn13).valid?.should be_true
+      end
+    end
 
-     it "should not validate if seed looks like an ISBN-13 but has an invalid check digit" do
-       isbns do |isbn10, isbn13|
-         inv = isbn13.gsub(/(.)$/, "#{(isbn13.split(//).last.to_i - 2) % 10}")
-         lambda { ISBN.new(inv) }.should raise_error ISBNError
-       end
-     end
+    it "should not validate if seed looks like an ISBN-13 but has an invalid check digit" do
+      isbns do |isbn10, isbn13|
+        invalid = isbn13.gsub(/(.)$/, "#{(isbn13.split(//).last.to_i - 2) % 10}")
+        ISBN.new(invalid).valid?.should be_false
+      end
+    end
 
-     it "should not validate if seed looks like an ISBN-10 but has an invalid check digit" do
-       isbns do |isbn10, isbn13|
-         inv = isbn10.gsub(/(.)$/, "#{(isbn10.split(//).last.to_i - 2) % 10}")
-         lambda { ISBN.new(inv) }.should raise_error ISBNError
-       end
-     end
+    it "should not validate if seed looks like an ISBN-10 but has an invalid check digit" do
+      isbns do |isbn10, isbn13|
+        invalid = isbn10.gsub(/(.)$/, "#{(isbn10.split(//).last.to_i - 2) % 10}")
+        ISBN.new(invalid).valid?.should be_false
+      end
+    end
 
-     it "should not validate if seed is not an ISBN" do
-       %w{foo 1}.each { |seed| lambda { ISBN.new(seed) }.should raise_error ISBNError }
-     end
+    it "should not validate if seed is not an ISBN" do
+      ['foo', 1, '', nil].each { |seed| ISBN.new(seed).valid?.should be_false }
+    end
 
-     it "should not validate if seed is blank" do
-       lambda { ISBN.new('') }.should raise_error ISBNError
-     end
+    it "should cast as string" do
+      Bookland::ISBN.new('0262011530').to_s.should == '0262011530'
+    end
 
-     it "should not validate if seed is nil" do
-       lambda { ISBN.new(nil) }.should raise_error ISBNError
-     end
+    it "should cast as string and hyphenate an ISBN-13" do
+      ISBN.new("9780485113358").to_s(3, 10).should == '978-0485113358'
+    end
 
-     it "should cast as string" do
-       Bookland::ISBN.new('0262011530').to_s.should == '0262011530'
-     end
+    it "should cast as string and hyphenate an ISBN-10" do
+      ISBN.new("048511335X").to_s(1, 3, 5, 1).should == '0-485-11335-X'
+    end
 
-     it "should cast as string and hyphenate an ISBN-13" do
-       ISBN.new("9780485113358").to_s(3, 10).should == '978-0485113358'
-     end
+    it "should raise an error if an invalid ISBN is cast as string" do
+      ['foo', 1, '', nil].each { |seed| lambda { ISBN.new(seed).to_s }.should raise_error ISBNError }
+    end
 
-     it "should cast as string and hyphenate an ISBN-10" do
-       ISBN.new("048511335X").to_s(1, 3, 5, 1).should == '0-485-11335-X'
-     end
+    it "should raise an error if an invalid ISBN-13 is converted to ISBN-10" do
+      isbns do |isbn10, isbn13|
+        invalid = isbn13.gsub(/(.)$/, "#{(isbn13.split(//).last.to_i - 2) % 10}")
+        lambda { ISBN.new(invalid).to_isbn10 }.should raise_error ISBNError
+      end
+    end
+
+    it "should raise an error if an invalid ISBN-10 is converted to ISBN-13" do
+      isbns do |isbn10, isbn13|
+        invalid = isbn10.gsub(/(.)$/, "#{(isbn10.split(//).last.to_i - 2) % 10}")
+        lambda { ISBN.new(invalid).to_isbn13 }.should raise_error ISBNError
+      end
+    end
   end
 end
