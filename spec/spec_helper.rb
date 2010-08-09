305b8892d6aa91e5a1164e94d50ce50cc2ab53a5
Hakan Ensari <hakan.ensari@papercavalier.com>
2010-08-09 17:03:54 +0100

    Test helpers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,12 @@
-require File.expand_path("../../lib/bookland", __FILE__)
+unless defined?(Bundler)
+  require "rubygems"
+  require "bundler/setup"
+end
 
+require "rspec"
 require "yaml"
+
+require File.expand_path("../../lib/bookland", __FILE__)
 
 def isbns
   File.open(File.expand_path("../fixtures/isbn", __FILE__)).each do |line|
