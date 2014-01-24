e36e14b98c6e38a51193c0ea4b7e04442ad40ece
aaron <yalong1976@gmail.com>
2014-01-24 09:13:51 +0800

    refactor validators

--- lib/bookland/validators.rb
+++ lib/bookland/validators.rb
@@ -1,16 +1,28 @@
 %w(ASIN EAN ISBN).each do |klass|
   Object.class_eval <<-EOF
-    class #{klass.capitalize}Validator < ActiveModel::EachValidator
-      def validate_each(record, attribute, value)
-        unless Bookland::#{klass}.valid?(value)
-          message = "is not an #{klass}"
-          if options[:strict]
-            raise ActiveModel::StrictValidationFailed, "\#{value} \#{message}"
-          else
-            record.errors[attribute] << message
+    module ValidationExtensions
+      class #{klass.capitalize}Validator < ActiveModel::EachValidator
+        def initialize(options)
+          options[:message]     ||= "is not a valid #{klass} code"
+          options[:allow_nil]   ||= false
+          options[:allow_blank] ||= false
+          super(options)
+        end
+
+        def validate_each(record, attribute, value)
+          unless Bookland::#{klass}.valid?(value) || (value.nil? && options[:allow_nil]) || (value.blank? && options[:allow_blank])
+            record.errors.add(attribute, options[:message])
           end
         end
+      end
+
+      module HelperMethods
+        def validates_#{klass.downcase}(*attr_names)
+          validates_with #{klass.capitalize}Validator, _merge_attributes(attr_names)
+        end        
       end
     end
   EOF
 end
+
+ActiveModel::Validations::HelperMethods.send(:include, ValidationExtensions::HelperMethods)
