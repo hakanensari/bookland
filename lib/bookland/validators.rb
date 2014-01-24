8ff5f2d179f5706b33b1525852b80d4fdcd60dbf
aaron <yalong1976@gmail.com>
2014-01-24 10:58:37 +0800

    Compatible ISBN10 && allow "-" to separate

--- lib/bookland/validators.rb
+++ lib/bookland/validators.rb
@@ -1,4 +1,4 @@
-%w(ASIN EAN ISBN).each do |klass|
+%w(ASIN EAN).each do |klass|
   Object.class_eval <<-EOF
     module ValidationExtensions
       class #{klass.capitalize}Validator < ActiveModel::EachValidator
@@ -23,6 +23,31 @@
       end
     end
   EOF
+end
+
+module ValidationExtensions
+  class IsbnValidator < ActiveModel::EachValidator
+    def initialize(options)
+      options[:message]     ||= "is not a valid ISBN code"
+      options[:allow_nil]   ||= false
+      options[:allow_blank] ||= false
+      options[:compat] ||= true
+      super(options)
+    end
+
+    def validate_each(record, attribute, value)
+      return if  (value.nil? && options[:allow_nil]) || (value.blank? && options[:allow_blank])
+      unless Bookland::ISBN.valid?(value) || (options[:compact] && Bookland::ISBN10.valid?(value))
+        record.errors.add(attribute, options[:message])
+      end
+    end
+  end
+
+  module HelperMethods
+    def validates_isbn(*attr_names)
+      validates_with IsbnValidator, _merge_attributes(attr_names)
+    end        
+  end  
 end
 
 ActiveModel::Validations::HelperMethods.send(:include, ValidationExtensions::HelperMethods)
